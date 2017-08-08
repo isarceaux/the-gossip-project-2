a18768548072e52f84e941d0037ed5df76245498
isarceaux <isarceaux>
2017-08-08 17:29:25 +0200

    index hidden for people not logged in + flash css of devise done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :sanitize_devise_params, if: :devise_controller?
 
-  include ApplicationHelper
-
   def sanitize_devise_params
       devise_parameter_sanitizer.permit(:sign_up, keys: [:anonymous_username])
   end
