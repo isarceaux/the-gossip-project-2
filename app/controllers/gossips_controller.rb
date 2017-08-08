6ead88ae74c5bc21ea5338e77b66278925da61ea
hong <luu.bichhong@gmail.com>
2017-08-08 16:31:31 +0200

    gossip creation finish

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,16 +1,40 @@
 class GossipsController < ApplicationController
   def new
+    @gossip = Gossip.new
+  end
+
+  def create
+    @gossip = Gossip.new(gossip_params)
+    @gossip.corsaire = current_corsaire
+    if @gossip.save  
+      redirect_to @gossip
+    else
+      render :new
+    end
   end
 
   def edit
+
+  end
+
+  def update
+
   end
 
   def destroy
   end
 
   def index
+    @gossips = Gossip.all
   end
 
   def show
+    @gossip = Gossip.find(params[:id])
   end
+
+  private
+    def gossip_params
+      params.require(:gossip).permit(:content)
+    end
+
 end
